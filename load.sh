444876c972c4cb87437c9e77b567e63fadabb93e
Pavan Kumar Yuganathan <vipinkopavan@gmail.com>
2025-04-21 15:32:56 +0530

    added shell script to increase load

---
+++ load.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Number of concurrent requests
+NUM_REQUESTS=100
+
+# Loop to send concurrent requests using curl in the background
+for i in $(seq 1 $NUM_REQUESTS); do
+  curl http://localhost:3000 &
+done
+
+# Wait for all background processes to complete
+wait
+
+echo "All requests sent"
