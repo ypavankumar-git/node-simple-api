8a8dfc9b802868e939d4f478f019d7850862eb34
Pavan Kumar Yuganathan <vipinkopavan@gmail.com>
2025-04-21 17:49:27 +0530

    Changed curl url to minikube ip

--- load.sh
+++ load.sh
@@ -5,7 +5,7 @@ NUM_REQUESTS=100000
 
 # Loop to send concurrent requests using curl in the background
 for i in $(seq 1 $NUM_REQUESTS); do
-  curl http://localhost:3000 &
+  curl http://192.168.49.2:30001 &
 done
 
 # Wait for all background processes to complete
